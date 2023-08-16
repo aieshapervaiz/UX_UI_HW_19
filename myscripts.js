8d2fc29c5ef8314fddb6440ac6154abbbfba45b6
aieshapervaiz <ayeshapervaiz1914@gmail.com>
2023-08-16 16:13:12 -0400

    refresh

--- myscripts.js
+++ myscripts.js
@@ -1,29 +1,29 @@
-console.log ("Your index.js file is loaded correctly!");
 /*Preloader*/
 setTimeout (function(){
     $('.loader_bg').fadeToggle();
  }, 1500);
 
+/* Scroll back to top*/
  let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress-value");
+
     let pos = document.documentElement.scrollTop;
-    let calcHeight = 
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if(pos>100){
-        scrollProgress.style.display = "grid";
-    } else {
-        scrollProgress.style.display = "none";
-    }
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = 'conic-gradient (#981724 $(scrollValue)%, #F4EAE9 $(scrollValue)%)';
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
+   let scrollValue = Math.round ((pos * 100) / calcHeight);
+   if (pos>100){
+       scrollProgress.style.display = 'grid';
+   } else {
+       scrollProgress.style.display = "none";
+   }
+   scrollProgress.addEventListener("click", () => {
+       document.documentElement.scrollTop = 0;
+   });
+  
+  scrollProgress.style.background = 'conic-gradient (#981724 ${scrollValue}%, #F4EAE9 ${scrollValue}%)';
 
- };
-
+};
+    
  window.onscroll = calcScrollValue;
  window.onload = calcScrollValue;
