9673843d914fe62d741e5a210c32dacd9dd13345
aieshapervaiz <ayeshapervaiz1914@gmail.com>
2023-08-17 01:22:20 -0400

    refresh

--- myscripts.js
+++ myscripts.js
@@ -1,43 +1,18 @@
-//scroll reveal
- //header
- ScrollReveal().reveal('.logo', { delay: 500, origin: 'left' });
- ScrollReveal().reveal('.navbar', { delay: 600, origin: 'bottom' });
- ScrollReveal().reveal('.cta', { delay: 700, origin: "right" });
- 
-
- //hero banner
- ScrollReveal().reveal('chibbi', { easing: "ease-in" });
- ScrollReveal().reveal('.name', { easing: "ease-in" });
- ScrollReveal().reveal('.intro', { easing: "ease-in" });
- ScrollReveal().reveal('#button1', { easing: "ease-in" });
- ScrollReveal().reveal('#button2', { easing: "ease-in" });
-
- //Gradient text
- ScrollReveal().reveal('.grad1', { easing: "ease-in" });
- ScrollReveal().reveal('#wrapper-text', { easing: "ease-in" });
- ScrollReveal().reveal('#wrapper', { easing: "ease-in" });
-
- //recent work
- ScrollReveal().reveal('.recentwork', { easing: "ease-in" });
- ScrollReveal().reveal('.para1', { easing: "ease-in" });
- ScrollReveal().reveal('.para2', { easing: "ease-in" });
- 
- 
- // Preloader
+// Preloader
 setTimeout (function(){
     $('.loader_bg').fadeToggle();
  }, 1500);
 
 
 // Scroll back to top
- let calcScrollValue = () => {
+    let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress-value");
 
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-   let scrollValue = Math.round ((pos * 100) / calcHeight);
+    let scrollValue = Math.round ((pos * 100) / calcHeight);
    if (pos>100){
        scrollProgress.style.display = "grid";
    } else {
@@ -47,13 +22,12 @@ setTimeout (function(){
        document.documentElement.scrollTop = 0;
    });
   
-  scrollProgress.style.background = 'conic-gradient (#981724 ${scrollValue}%, #F4EAE9 ${scrollValue}%)';
+   scrollProgress.style.background = 'conic-gradient (#981724 ${scrollValue}%, #F4EAE9 ${scrollValue}%)';
 
 };
     
  window.onscroll = calcScrollValue;
  window.onload = calcScrollValue;
-
 
  
 
