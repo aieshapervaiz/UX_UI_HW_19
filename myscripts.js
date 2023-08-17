643200958296b3205ec8877116be066ed45d60be
aieshapervaiz <ayeshapervaiz1914@gmail.com>
2023-08-17 00:28:37 -0400

    refresh

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,35 @@
-/*Preloader*/
+//scroll reveal
+ //header
+ ScrollReveal().reveal('.logo', { delay: 500, origin: 'left' });
+ ScrollReveal().reveal('.navbar', { delay: 600, origin: 'bottom' });
+ ScrollReveal().reveal('.cta', { delay: 700, origin: "right" });
+ 
+
+ //hero banner
+ ScrollReveal().reveal('chibbi', { easing: "ease-in" });
+ ScrollReveal().reveal('.name', { easing: "ease-in" });
+ ScrollReveal().reveal('.intro', { easing: "ease-in" });
+ ScrollReveal().reveal('#button1', { easing: "ease-in" });
+ ScrollReveal().reveal('#button2', { easing: "ease-in" });
+
+ //Gradient text
+ ScrollReveal().reveal('.grad1', { easing: "ease-in" });
+ ScrollReveal().reveal('#wrapper-text', { easing: "ease-in" });
+ ScrollReveal().reveal('#wrapper', { easing: "ease-in" });
+
+ //recent work
+ ScrollReveal().reveal('.recentwork', { easing: "ease-in" });
+ ScrollReveal().reveal('.para1', { easing: "ease-in" });
+ ScrollReveal().reveal('.para2', { easing: "ease-in" });
+ 
+ 
+ // Preloader
 setTimeout (function(){
     $('.loader_bg').fadeToggle();
  }, 1500);
 
-/* Scroll back to top*/
+
+// Scroll back to top
  let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress-value");
@@ -13,7 +39,7 @@ setTimeout (function(){
 
    let scrollValue = Math.round ((pos * 100) / calcHeight);
    if (pos>100){
-       scrollProgress.style.display = 'grid';
+       scrollProgress.style.display = "grid";
    } else {
        scrollProgress.style.display = "none";
    }
@@ -27,3 +53,7 @@ setTimeout (function(){
     
  window.onscroll = calcScrollValue;
  window.onload = calcScrollValue;
+
+
+ 
+
